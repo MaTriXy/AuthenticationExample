c192fe673e3fc211a31860d2ef03db1f2e882438
Yossi Elkrief <elkriefy@gmail.com>
2015-09-28 17:16:44 +0300

    usesCleartextTraffic
    Fixed usesCleartextTraffic sample

--- app/src/main/java/com/elkriefy/android/apps/authenticationexample/cleartextnetworkusage/ClearTextNetworkUsageActivity.java
+++ app/src/main/java/com/elkriefy/android/apps/authenticationexample/cleartextnetworkusage/ClearTextNetworkUsageActivity.java
@@ -2,28 +2,48 @@ package com.elkriefy.android.apps.authenticationexample.cleartextnetworkusage;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.webkit.WebView;
-import android.widget.TextView;
+import android.widget.ImageView;
 
+import com.elkriefy.android.apps.authenticationexample.BuildConfig;
 import com.elkriefy.android.apps.authenticationexample.R;
+import com.squareup.picasso.Picasso;
 
 /**
- * Main entry point for the sample, showing a backpack and "Purchase" button.
+ * This is a sample showcase for showing the usesCleartextTraffic Flag.
  */
 public class ClearTextNetworkUsageActivity extends Activity {
 
+    /**
+     * This does not honor usesCleartextTraffic  flag at all - as mentioned in documentation.
+     */
     WebView mWebview;
 
-    TextView mTextview;
+    //We Will load the Image from the URL into this simple View.
+    ImageView mImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        ((AuthenticationExample) getApplication()).inject(this);
-
+        if (BuildConfig.APPLY_STRICT_MODE) {
+            StrictMode.VmPolicy.Builder b = new StrictMode.VmPolicy.Builder();
+            b.detectCleartextNetwork().penaltyLog();
+            StrictMode.setVmPolicy(b.build());
+        }
         setContentView(R.layout.clear_text_network_activity_main);
-        mWebview = (WebView) findViewById(R.id.WebView_Network_test);
+        mImageView = (ImageView) findViewById(R.id.imageViewOutCome);
 
+        mWebview = (WebView) findViewById(R.id.WebView_Network_test);
+        mWebview.loadUrl(BuildConfig.URL);
+
+        try {
+            Picasso.with(this).load(BuildConfig.URL).fit().into(mImageView);
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
 
     }
 
